5158ee6702a847f5fa52d600f171465833cea0ed
MchaseCov <67227108+MchaseCov@users.noreply.github.com>
2022-01-30 16:02:24 -0500

    feat: remove item note

--- Core.lua
+++ Core.lua
@@ -22,21 +22,44 @@ function ItemNotes:SetDefaults()
     end
 end
 
-function ItemNotes:SetNoteHandler(input)
-    local _, _, item, note = string.find(input, "%s?(%w+)%s?(.*)")
+function ItemNotes:ReturnSetNoteMessage(item, note)
+    local note = note
+    local item = Item:CreateFromItemID((tonumber(item)))
+    item:ContinueOnItemLoad(function()
+        local itemLink = item:GetItemLink()
+        print("Set " .. itemLink .. " to " .. note)
+    end)
+end
+
+function ItemNotes:SetItemNote(item, note)
 
     if note and #note > 0 then
         ItemNotes:SetNote(item, note)
-        local itemName, itemLink = GetItemInfo(item)
-        print("Set " .. itemLink .. " to " .. note)
+        ItemNotes:ReturnSetNoteMessage(item, note)
     else
         print("You must type a note!")
     end
+end
+
+function ItemNotes:RemoveItemNote(item)
+    if self.db.notes and item then
+        self.db.notes[item] = nil
+        print("Removed note")
+    end
+end
+
+function ItemNotes:NoteCommandHandler(input)
+    local _, _, cmd, args = string.find(input, "%s?(%w+)%s?(.*)")
+    local _, _, item, note = string.find(args, "%s?(%w+)%s?(.*)")
+
+    if cmd == "add" and args ~= "" then ItemNotes:SetItemNote(item, note) end
+    if cmd == "remove" and args ~= "" then ItemNotes:RemoveItemNote(item) end
 end
 
 function ItemNotes:SetNote(item, note)
     if self.db.notes and item then self.db.notes[item] = note end
 end
 
-SLASH_ADDNOTE1 = "/itemnoteadd"
-SlashCmdList["ADDNOTE"] = function(msg) ItemNotes:SetNoteHandler(msg) end
+SLASH_ITEMNOTE1 = "/itemnote"
+SlashCmdList["ITEMNOTE"] =
+    function(input) ItemNotes:NoteCommandHandler(input) end
